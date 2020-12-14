94800b6c7f37052439f9f7084024b5c422b54ed2
EduSobre <47386850+EduSobre@users.noreply.github.com>
2020-12-15 00:15:32 +0100

    Instrucciones

---
+++ Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargarInstrucciones()
+    {
+        SceneManager.LoadScene("Instrucciones");    
+    }
+
+    public void CargarPrincipal()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
